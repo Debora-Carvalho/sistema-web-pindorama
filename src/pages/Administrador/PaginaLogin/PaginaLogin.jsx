5c60982e0c39fef01381c3b07f9f3be059510e71
Gustavo Bongiovani <guhhfive@outlook.com>
2025-09-18 00:25:55 -0300

    Semi finalização da tela de login

--- src/pages/Administrador/PaginaLogin/PaginaLogin.jsx
+++ src/pages/Administrador/PaginaLogin/PaginaLogin.jsx
@@ -1,48 +1,64 @@
 import styles from './PaginaLogin.module.scss';
 import useTituloDocumento from '../../../hooks/useTituloDocumento.js';
-import cachorro from '../../../assets/images/CachorroPaginaLogin.jpg'
+import cachorro from '../../../assets/images/CachorroPaginaLogin.png';
 import { FaArrowCircleRight } from "react-icons/fa";
 import { IoArrowBackCircleOutline } from "react-icons/io5";
+import { IoEyeSharp } from "react-icons/io5";
+import { BsEyeSlashFill } from "react-icons/bs";
+import { useState } from 'react';
 
 
 function PaginaLogin() {
   useTituloDocumento("Login | Pindorama");
 
+  const [mostrarSenha, setMostrarSenha] = useState(false);
   return (
     <>
-	<div className={styles.containerLogin}>
-	<div className={styles.columDoguinho}> 
-	 <img className={styles.doguinho} src={cachorro} alt="Cachorro caramelo" />
-	</div>
-      <div className={styles.columForm}>
-	    <button onClick={() => window.location.href = "/pagina-inicial-usuario"}>
-         <IoArrowBackCircleOutline className={styles.btnVoltarInicial}/>
-        </button>
-        <p className={styles.tituloLogin}>Bem vinda ao Pindorama</p>
-        <p className={styles.word}>Digite sua senha para entrar</p>
-
-        <div className={styles.sectionSenha}>
-          <label htmlFor="senha"></label>
-          <input  
-		    className={styles.inputLogin}
-            type="password"
-            id="senha"
-            name="senha"
-            minLength="6"
-            required
-          />
-          <button className={styles.btnLogin} 
-		     onClick={() => window.location.href = "/pagina-inicial-admin"}>
-		     < FaArrowCircleRight className={styles.btnEntrarLogin}/>
-          </button>
+      <div className={styles.containerLogin}>
+        <div className={styles.columDoguinho}>
+          <img className={styles.doguinho} src={cachorro} alt="Cachorro caramelo" />
         </div>
+        <div className={styles.columForm}>
+          <button onClick={() => window.location.href = "/pagina-inicial-usuario"}>
+            <abbr title="Voltar para a página inicial de Pindorama">
+              <IoArrowBackCircleOutline className={styles.btnVoltarInicial} />
+            </abbr>
+          </button>
+          <p className={styles.tituloLogin}>Bem vinda ao Pindorama</p>
+          <p className={styles.word}>Digite sua senha para entrar</p>
 
-        <div className={styles.resetSenha}>
-          <p >Esqueceu sua senha?</p>
-          <a href="/redefinir-senha">Redefinir senha</a>
+          <div className={styles.sectionSenha}>
+            <label htmlFor="senha"></label>
+            <input
+              className={styles.inputLogin}
+              type={mostrarSenha ? "text" : "password"}
+              id="senha"
+              name="senha"
+              minLength="6"
+              required
+            />
+            <button
+              type="button"
+              onClick={() => setMostrarSenha(!mostrarSenha)}
+              className={styles.btnMostrarSenha}
+            >
+              {mostrarSenha ? <IoEyeSharp /> : <BsEyeSlashFill />}
+            </button>
+
+            <button
+              className={styles.btnLogin}
+              onClick={() => window.location.href = "/pagina-inicial-admin"}
+            >
+              <FaArrowCircleRight className={styles.btnEntrarLogin} />
+            </button>
+          </div>
+
+          <div className={styles.resetSenha}>
+            <p >Esqueceu sua senha?</p>
+            <a className={styles.linkRedefinir} href="/redefinir-senha">Redefinir senha</a>
+          </div>
         </div>
       </div>
-</div>
     </>
   );
 }
