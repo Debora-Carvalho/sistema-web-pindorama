658f847e7e6ca522f545a84b7ec30859c82e1dca
Debora-Carvalho <deboravolc2@gmail.com>
2025-09-21 21:31:00 -0300

    ✨ adiciona componentes de acessibilidade no Header de usuário

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import { useNavigate, useLocation } from 'react-router-dom';
 
 import LogoPindorama from "../../assets/images/pindorama_logo5.png";
 
+import { CgSearch } from "react-icons/cg";
+import GroupBtnAcessibilidade from './GroupBtnAcessibilidade/GroupBtnAcessibilidade';
+
 function Header() {
 	const location = useLocation();
 	const navigate = useNavigate();
@@ -30,60 +33,68 @@ function Header() {
 	}
 
 	return (
-		<header className={styles.containerHeaderPrincipal}>
-			<div 
-				className={styles.logoHeaderPrincipal} 
-				style={{ cursor: "pointer" }} 
-				onClick={() => navigate("/")}
-			>
-				<img className={styles.logo} src={LogoPindorama} alt="Logo do site Pindorama" />
-			</div>
+		<header className={styles.container}>
+			<div className={styles.containerHeaderPrincipal}>
+				<div
+					className={styles.logoHeaderPrincipal}
+					style={{ cursor: "pointer" }}
+					onClick={() => navigate("/")}
+				>
+					<img className={styles.logo} src={LogoPindorama} alt="Logo do site Pindorama" />
+				</div>
 
-			<div className={styles.headerPrincipal}>
-				<div className={styles.headerPrincipalItems}>
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/inicio' ? styles.active : ''}`}
-						onClick={() => handleClick('inicio')}
-					>
-						Início
-					</button>
+				<div className={styles.headerPrincipal}>
+					<div className={styles.headerPrincipalItems}>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/inicio' ? styles.active : ''}`}
+							onClick={() => handleClick('inicio')}
+						>
+							Início
+						</button>
 
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/artigos' ? styles.active : ''}`}
-						onClick={() => handleClick('artigos')}
-					>
-						Artigos
-					</button>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/artigos' ? styles.active : ''}`}
+							onClick={() => handleClick('artigos')}
+						>
+							Artigos
+						</button>
 
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/eventos' ? styles.active : ''}`}
-						onClick={() => handleClick('eventos')}
-					>
-						Eventos
-					</button>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/eventos' ? styles.active : ''}`}
+							onClick={() => handleClick('eventos')}
+						>
+							Eventos
+						</button>
 
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/mapa' ? styles.active : ''}`}
-						onClick={() => handleClick('mapa')}
-					>
-						Mapa
-					</button>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/mapa' ? styles.active : ''}`}
+							onClick={() => handleClick('mapa')}
+						>
+							Mapa
+						</button>
 
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/organizacoes' ? styles.active : ''}`}
-						onClick={() => handleClick('organizacoes')}
-					>
-						Organizações
-					</button>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/organizacoes' ? styles.active : ''}`}
+							onClick={() => handleClick('organizacoes')}
+						>
+							Organizações
+						</button>
 
-					<button
-						className={`${styles['headerBtn']} ${location.pathname === '/sobre' ? styles.active : ''}`}
-						onClick={() => handleClick('sobre')}
-					>
-						Sobre mim
-					</button>
+						<button
+							className={`${styles['headerBtn']} ${location.pathname === '/sobre' ? styles.active : ''}`}
+							onClick={() => handleClick('sobre')}
+						>
+							Sobre mim
+						</button>
+
+						<div>
+							<CgSearch className={styles.btnSearch} />
+						</div>
+					</div>
 				</div>
 			</div>
+
+			<GroupBtnAcessibilidade />
 		</header>
 	);
 }
