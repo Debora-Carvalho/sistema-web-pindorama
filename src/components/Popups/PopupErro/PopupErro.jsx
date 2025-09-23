9f3d609495366d643586f23a62b771aedb207dd4
BONJIU <santos.bongiovani@gmail.com>
2025-09-22 22:07:26 -0300

    :goal_net: feat: Incluindo tratamento de erro de login e demais erros em telas

--- src/components/Popups/PopupErro/PopupErro.jsx
+++ src/components/Popups/PopupErro/PopupErro.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './PopupErro.module.scss';
 
-function PopupErro({ aberto, mensagem }) {
+function PopupErro({ aberto, mensagem, tipo = 'padrao' }) {
   if (!aberto) return null;
 
   return (
     <div className={styles.popupOverlayErro}>
-      <div className={styles.popupBoxErro}>
+      <div className={`${styles.popupBoxErro} ${styles[tipo]}`}>
         <p>{mensagem}</p>
       </div>
     </div>
   );
 }
+
 
 export default PopupErro;
