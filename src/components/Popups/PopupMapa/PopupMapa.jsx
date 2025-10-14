487a88769142ce532d738807f37bac64bd84fe36
Debora-Carvalho <deboravolc2@gmail.com>
2025-10-14 19:02:45 -0300

    ✨ estabelece limites para a interação com o popup do mapa

--- src/components/Popups/PopupMapa/PopupMapa.jsx
+++ src/components/Popups/PopupMapa/PopupMapa.jsx
@@ -5,13 +5,21 @@ import { AiOutlineClose } from "react-icons/ai";
 import styles from './PopupMapa.module.scss';
 
 function PopupMapa({ aberto, titulo, descricao, textoBotao, linkDestino, onFechar }) {
-    const nodeRef = useRef(null); // ref para o Draggable
+    const nodeRef = useRef(null); //ref para o Draggable
 
     if (!aberto) return null;
 
     return (
         <div className={styles.popupOverlayMapa}>
-            <Draggable nodeRef={nodeRef} handle={`.${styles.tituloDraggable}`}>
+            <Draggable 
+                nodeRef={nodeRef} 
+                handle={`.${styles.tituloDraggable}`}
+                bounds={{
+                    left: 0,
+                    right: window.innerWidth - 540,  
+                    bottom: window.innerHeight - 800
+                }}
+            >
                 <div ref={nodeRef} className={styles.popupBoxMapa}>
                     <div className={styles.tituloDraggable}>
                         <h3>{titulo}</h3>
